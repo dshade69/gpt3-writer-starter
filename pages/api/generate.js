150b1e136bf7d36b2a9caa56217f64cb412aa642
dshade69 <dshade69@gmail.com>
2022-11-28 13:44:22 -0600

    Add files via upload

---
+++ pages/api/generate.js
@@ -0,0 +1,28 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = 'Write a poem in the style of ${req.body.authorInput} about ${req.body.promptInput}'
+
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.authorInput}${req.body.promptInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: `${basePromptPrefix}\n`,
+    temperature: 0.8,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+
+export default generateAction;
